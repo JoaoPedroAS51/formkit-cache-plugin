34222fc2e0582d8fddfe5f29b8a76839e7068ca9
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-14 06:51:44 -0300

    feat: cache plugin for local storage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import type{ FormKitNode, FormKitPlugin } from '@formkit/core'
-
+import type { FormKitNode, FormKitPlugin } from '@formkit/core'
+import { undefine } from '@formkit/utils'
 
 /**
  * The options to be passed to {@link createCachePlugin|createCachePlugin}
@@ -7,7 +7,8 @@ import type{ FormKitNode, FormKitPlugin } from '@formkit/core'
  * @public
  */
 export interface CacheOptions {
-  //
+  prefix?: string
+  maxAge?: number
 }
 
 /**
@@ -21,7 +22,46 @@ export interface CacheOptions {
  */
 export function createCachePlugin(CacheOptions?: CacheOptions): FormKitPlugin {
   const cachePlugin = (node: FormKitNode) => {
-    //
+    if (node.props.type !== 'form') {
+      return
+    }
+    node.addProps(['cache'])
+
+    node.on('created', () => {
+      const cache = undefine(node.props.cache)
+      if (!cache) {
+        return
+      }
+
+      const prefix = CacheOptions?.prefix ?? 'formkit'
+      const maxAge = CacheOptions?.maxAge ?? 3_600_000 // 1 hour
+      const key = `${prefix}-${node.name}`
+      const value = localStorage.getItem(key)
+
+      if (value) {
+        const localStorageValue = JSON.parse(value)
+        if (localStorageValue.maxAge > Date.now()) {
+          node.input(localStorageValue.data)
+        } else {
+          localStorage.removeItem(key)
+        }
+      }
+
+      node.on('commit', ({ payload }) => {
+        localStorage.setItem(
+          key,
+          JSON.stringify({
+            maxAge: Date.now() + maxAge,
+            data: payload,
+          })
+        )
+      })
+
+      node.hook.submit((payload, next) => {
+        localStorage.removeItem(key)
+        return next(payload)
+      })
+    })
   }
 
   return cachePlugin
