af1cb3c027b71d9b58d8f46c3dc13b792691207a
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-17 00:35:24 -0300

    demo: update example

--- playground/src/formkit.config.ts
+++ playground/src/formkit.config.ts
@@ -1,16 +1,9 @@
 import '@formkit/themes/genesis'
 import { genesisIcons } from '@formkit/icons'
-import { DefaultConfigOptions } from '@formkit/vue'
-import { createCachePlugin } from '../..'
-import localStorageDriver from 'unstorage/drivers/localstorage'
+import type { DefaultConfigOptions } from '@formkit/vue'
 
 const config: DefaultConfigOptions = {
-  icons: { ...genesisIcons },
-  plugins: [
-    createCachePlugin({
-      driver: localStorageDriver()
-    })
-  ]
+  icons: { ...genesisIcons }
 }
 
 export default config
